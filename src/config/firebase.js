02272f9c975a3e6c78fd413bf14e9914885cf265
Michel Pomerantzeff <micheldauniao@hotmail.com>
2023-12-05 22:35:43 +0000

    test: minor changes

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -10,11 +10,11 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "movie-app-4e224.firebaseapp.com",
-  projectId: "movie-app-4e224",
-  storageBucket: "movie-app-4e224.appspot.com",
-  messagingSenderId: "579218603210",
-  appId: "1:579218603210:web:85554f56b082dacdda4833"
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
